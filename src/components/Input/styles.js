d924a61bea7567c6d6a6839831f80973d4506571
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2022-12-06 17:56:12 -0300

    finalizando

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -5,21 +5,23 @@ export const InputContainer = styled.div`
     height: 75px;
     background-color: #AAAAFF;
 
+
     display: flex;
-    align-items: center;
     justify-content: flex-end;
+    align-items: center;
 
     font-size: 24px;
     font-family: 'Roboto';
-
+    
     input {
         width: 100%;
         height: 75px;
         background-color: #a99f93;
         border: 0;
         padding: 0 10px;
+        text-align: right;
 
-        font-size: 24px;
+        font-size: 30px;
         font-family: 'Roboto';
         color: #745141;
     }
